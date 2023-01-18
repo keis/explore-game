d9a5c40da4c8208d643b9dce4869ebd91eda915a
David Keijser <keijser@gmail.com>
2023-01-18 22:02:04 +0100

    hexgrid: Add iterator over ring coordinates

--- src/hexgrid/mod.rs
+++ src/hexgrid/mod.rs
@@ -2,9 +2,11 @@ mod coord;
 mod grid;
 pub mod imat3;
 pub mod layout;
+mod ring;
 mod transform;
 
 pub use coord::HexCoord;
 pub use grid::Grid;
 pub use layout::GridLayout;
+pub use ring::ring;
 pub use transform::{Transform, TransformMatrix};
