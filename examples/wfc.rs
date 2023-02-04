e3c8b5482986f9074e8f87dd61839bd0b2984dd2
David Keijser <keijser@gmail.com>
2023-02-04 21:40:03 +0100

    wfc: Add cli options to control size and seed in example

--- examples/wfc.rs
+++ examples/wfc.rs
@@ -1,3 +1,4 @@
+use clap::{Args, Parser, Subcommand};
 use explore_game::{
     hexgrid::layout::{HexagonalGridLayout, SquareGridLayout},
     hexgrid::{Grid, GridLayout},
@@ -6,7 +7,7 @@ use explore_game::{
         cell::Cell,
         tile::{extract_tiles, standard_tile_transforms},
         util::{wrap_grid, DumpGrid, DumpGridWith, LoadGrid},
-        Generator, Template,
+        Generator, Seed, SeedType, Template,
     },
 };
 use std::fs::File;
@@ -20,33 +21,82 @@ fn sample_map() -> Result<Grid<HexagonalGridLayout, Terrain>, &'static str> {
 
 fn sample_template() -> Template<Terrain> {
     let input = sample_map().unwrap();
-    input.dump(&mut io::stdout()).unwrap();
     let wrapped_input = wrap_grid(input);
     let transforms = standard_tile_transforms();
     Template::from_tiles(extract_tiles(&wrapped_input, &transforms))
 }
 
-fn main() {
-    let template = sample_template();
-    println!("{:?}", template.stats());
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+    #[arg(global = true, long)]
+    seed: Option<Seed>,
+}
 
-    let mut generator = Generator::new_with_layout(
-        &template,
-        SquareGridLayout {
-            width: 10,
-            height: 8,
-        },
-    );
-    println!("map size is {:?}", generator.grid.layout.size());
+#[derive(Subcommand, Debug)]
+enum Command {
+    Hexagonal(HexagonalArgs),
+    Square(SquareArgs),
+}
+
+#[derive(Args, Debug)]
+struct HexagonalArgs {
+    radius: u16,
+}
+
+#[derive(Args, Debug)]
+struct SquareArgs {
+    width: u16,
+    height: u16,
+}
+
+fn generate_map<Layout>(template: &Template<Terrain>, generator: &mut Generator<Layout, Terrain>)
+where
+    Layout: GridLayout,
+    Grid<Layout, Terrain>: DumpGrid,
+    Grid<Layout, Cell>: DumpGridWith<Item = Cell>,
+{
     while generator.step().is_some() {
         generator
             .grid
             .dump_with(&mut io::stdout(), |cell| match cell {
                 Cell::Collapsed(tile) => template.contribution(*tile).into(),
-                Cell::Alternatives(_, _) => '?',
+                Cell::Alternatives(alts, _) if alts < &template.available_tiles() => '?',
+                Cell::Alternatives(_, _) => '.',
             })
             .unwrap();
     }
     let output = generator.export().unwrap();
     output.dump(&mut io::stdout()).unwrap();
+}
+
+fn main() -> Result<(), &'static str> {
+    let args = Cli::parse();
+
+    let template = sample_template();
+    println!("{:?}", template.stats());
+
+    match args.command {
+        Command::Hexagonal(params) => {
+            let seed = args
+                .seed
+                .unwrap_or_else(|| Seed::new(SeedType::Hexagonal(params.radius)));
+            println!("Generating map with seed {}", seed);
+            let mut generator: Generator<HexagonalGridLayout, Terrain> =
+                Generator::new_with_seed(&template, seed)?;
+            generate_map(&template, &mut generator);
+            Ok(())
+        }
+        Command::Square(params) => {
+            let seed = args
+                .seed
+                .unwrap_or_else(|| Seed::new(SeedType::Square(params.width, params.height)));
+            println!("Generating map with seed {}", seed);
+            let mut generator: Generator<SquareGridLayout, Terrain> =
+                Generator::new_with_seed(&template, seed)?;
+            generate_map(&template, &mut generator);
+            Ok(())
+        }
+    }
 }
