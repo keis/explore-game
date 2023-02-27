97d7dbdf917c957bc038fa6f6b65d68b7d4df407
David Keijser <keijser@gmail.com>
2023-02-27 18:52:49 +0100

    wfc: Use fixedbitset in generator
    This shows a 19% reduction in mean time to generate a hexagonal grid
    with radius 10 with a given but randomly selected seed while also
    reducing the standard deviation.
    
    Fixes #9

--- crates/expl_wfc/src/cell.rs
+++ crates/expl_wfc/src/cell.rs
@@ -1,56 +1,43 @@
 use super::TileId;
+use fixedbitset::FixedBitSet;
 use rand::Rng;
-use std::collections::HashSet;
 
 #[derive(Clone, Debug)]
 pub enum Cell {
     Collapsed(TileId),
-    Alternatives(usize, Vec<bool>),
+    Alternatives(usize, FixedBitSet),
 }
 
 impl Cell {
+    pub fn empty(num_alts: usize) -> Self {
+        let mut alts = FixedBitSet::with_capacity(num_alts);
+        alts.set_range(.., true);
+        Self::Alternatives(num_alts, alts)
+    }
+
     pub fn collapse<R: Rng + ?Sized>(&mut self, rng: &mut R) {
         if let Cell::Alternatives(num_alts, alts) = self {
             if *num_alts == 0usize {
                 return;
             }
             let choice = rng.gen_range(0..*num_alts);
-            if let Some(tile_id) = alts
-                .iter()
-                .enumerate()
-                .filter(|(_, x)| **x)
-                .nth(choice)
-                .map(|(i, _)| i as TileId)
-            {
+            if let Some(tile_id) = alts.ones().nth(choice).map(|i| i as TileId) {
                 *self = Cell::Collapsed(tile_id);
             }
         }
     }
 
-    pub fn set_alternatives(&mut self, alternatives: &HashSet<TileId>) {
+    pub fn set_alternatives(&mut self, alternatives: FixedBitSet) {
         if let Cell::Alternatives(num_alts, alts) = self {
-            for (idx, allowed) in alts.iter_mut().enumerate() {
-                *allowed = alternatives.contains(&idx);
-            }
-            *num_alts = alternatives.len();
+            *alts = alternatives;
+            *num_alts = alts.count_ones(..);
         }
     }
 
-    pub fn retain(&mut self, compatible: &HashSet<TileId>) {
+    pub fn retain(&mut self, compatible: &FixedBitSet) {
         if let Cell::Alternatives(num_alts, alts) = self {
-            for (idx, allowed) in alts.iter_mut().enumerate() {
-                *allowed = *allowed && compatible.contains(&idx);
-            }
-            *num_alts = alts.iter().filter(|allowed| **allowed).count();
-        }
-    }
-
-    pub fn reject<Iter: Iterator<Item = TileId>>(&mut self, rejected: Iter) {
-        if let Cell::Alternatives(num_alts, alts) = self {
-            for reject in rejected {
-                alts[reject] = false;
-            }
-            *num_alts = alts.iter().filter(|allowed| **allowed).count();
+            alts.intersect_with(compatible);
+            *num_alts = alts.count_ones(..);
         }
     }
 }
