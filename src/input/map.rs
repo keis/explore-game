5b3285b9caaff3215944f892f485059ff89eee95
David Keijser <keijser@gmail.com>
2023-07-15 13:28:51 +0200

    input: Limit move to primary button

--- src/input/map.rs
+++ src/input/map.rs
@@ -4,14 +4,21 @@ use crate::{
     map::{MapPosition, MapPresence, PathGuided, Zone},
 };
 use bevy::prelude::*;
+use bevy_mod_picking::prelude::{Click, Pointer, PointerButton};
 
-pub fn handle_zone_interaction(
-    zone_query: Query<(&MapPosition, &Interaction), (With<Zone>, Changed<Interaction>)>,
+pub fn handle_zone_click_events(
+    mut events: EventReader<Pointer<Click>>,
+    zone_query: Query<&MapPosition, With<Zone>>,
     presence_query: Query<(Entity, &Selection), (With<PathGuided>, With<MapPresence>)>,
     mut game_action_queue: ResMut<GameActionQueue>,
 ) {
-    let Ok((zone_position, Interaction::Clicked)) = zone_query.get_single() else { return };
-    for (entity, _) in presence_query.iter().filter(|(_, s)| s.is_selected) {
-        game_action_queue.add(GameAction::MoveTo(entity, zone_position.0));
+    for event in &mut events {
+        if event.event.button != PointerButton::Primary {
+            continue;
+        }
+        let Ok(zone_position) = zone_query.get(event.target) else { continue };
+        for (entity, _) in presence_query.iter().filter(|(_, s)| s.is_selected) {
+            game_action_queue.add(GameAction::MoveTo(entity, zone_position.0));
+        }
     }
 }
