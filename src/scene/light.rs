50c7b87e8057b6610db0ecd3eede70f1f0d7ab95
David Keijser <keijser@gmail.com>
2024-03-17 12:45:36 +0100

    scene: Add day/night light effects

--- src/scene/light.rs
+++ src/scene/light.rs
@@ -1,17 +1,102 @@
+use crate::turn::Period;
 use bevy::prelude::*;
+use bevy_tweening::{Animator, EaseFunction, Lens, Tracks, Tween};
+use std::time::Duration;
+
+// Yoinked from bevy_tweening
+trait ColorLerper {
+    fn lerp(&self, target: &Self, ratio: f32) -> Self;
+}
+
+impl ColorLerper for Color {
+    fn lerp(&self, target: &Color, ratio: f32) -> Color {
+        let r = self.r().lerp(target.r(), ratio);
+        let g = self.g().lerp(target.g(), ratio);
+        let b = self.b().lerp(target.b(), ratio);
+        let a = self.a().lerp(target.a(), ratio);
+        Color::rgba(r, g, b, a)
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct LightIlluminanceLens {
+    pub start: f32,
+    pub end: f32,
+}
+
+impl Lens<DirectionalLight> for LightIlluminanceLens {
+    fn lerp(&mut self, target: &mut DirectionalLight, ratio: f32) {
+        target.illuminance = self.start.lerp(self.end, ratio);
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct LightColorLens {
+    pub start: Color,
+    pub end: Color,
+}
+
+impl Lens<DirectionalLight> for LightColorLens {
+    fn lerp(&mut self, target: &mut DirectionalLight, ratio: f32) {
+        target.color = self.start.lerp(&self.end, ratio);
+    }
+}
 
 pub fn spawn_light(mut commands: Commands) {
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            illuminance: 14_000.0,
-            shadows_enabled: true,
+    commands.spawn((
+        Name::new("Lucifer"),
+        DirectionalLightBundle {
+            directional_light: DirectionalLight {
+                illuminance: 0.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, 10.0, 0.0),
+                rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_3),
+                ..default()
+            },
             ..default()
         },
-        transform: Transform {
-            translation: Vec3::new(0.0, 10.0, 0.0),
-            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_3),
-            ..default()
-        },
-        ..default()
-    });
+        Animator::new(Tween::new(
+            EaseFunction::QuadraticIn,
+            Duration::from_secs(2),
+            LightIlluminanceLens {
+                start: 0.0,
+                end: 11_0000.0,
+            },
+        )),
+    ));
+}
+
+pub fn apply_period_light(
+    period: Res<Period>,
+    mut light_query: Query<(&DirectionalLight, &mut Animator<DirectionalLight>)>,
+) {
+    let (light, mut animator) = light_query.single_mut();
+    let (illuminance, color) = match *period {
+        Period::Morning => (8_000.0, Color::rgb(1.0, 0.9, 0.9)),
+        Period::Day => (11_000.0, Color::rgb(1.0, 1.0, 1.0)),
+        Period::Evening => (10_000.0, Color::rgb(1.0, 0.8, 0.8)),
+        Period::Night => (6_000.0, Color::rgb(0.8, 0.8, 1.0)),
+    };
+    let tween = Tracks::new([
+        Tween::new(
+            EaseFunction::QuadraticInOut,
+            Duration::from_secs(2),
+            LightIlluminanceLens {
+                start: light.illuminance,
+                end: illuminance,
+            },
+        ),
+        Tween::new(
+            EaseFunction::QuadraticInOut,
+            Duration::from_secs(2),
+            LightColorLens {
+                start: light.color,
+                end: color,
+            },
+        ),
+    ]);
+    animator.set_tweenable(tween);
 }
--- src/turn.rs
+++ src/turn.rs
@@ -1,9 +1,32 @@
 use bevy::prelude::*;
 
-#[derive(Resource, Reflect, Default, Debug, Deref, DerefMut)]
+#[derive(Resource, Reflect, Copy, Clone, Default, Debug, Deref, DerefMut)]
 #[reflect(Resource)]
 pub struct Turn {
     pub number: u32,
+}
+
+#[derive(Resource, Reflect, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+#[reflect(Resource)]
+pub enum Period {
+    Morning,
+    #[default]
+    Day,
+    Evening,
+    Night,
+}
+
+impl From<Turn> for Period {
+    #[inline]
+    fn from(value: Turn) -> Self {
+        match value.number % 4 {
+            0 => Period::Day,
+            1 => Period::Evening,
+            2 => Period::Night,
+            3 => Period::Morning,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, States, Default)]
@@ -13,14 +36,30 @@ pub enum TurnState {
     Player,
 }
 
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+pub enum TurnSet {
+    Setup,
+    Effects,
+}
+
 pub struct TurnPlugin;
 
 impl Plugin for TurnPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<TurnState>()
             .register_type::<Turn>()
+            .insert_resource(Period::default())
             .insert_resource(Turn { number: 0 })
-            .add_systems(OnEnter(TurnState::Player), update_turn_counter);
+            .configure_sets(
+                OnEnter(TurnState::Player),
+                (TurnSet::Setup, TurnSet::Effects).chain(),
+            )
+            .add_systems(
+                OnEnter(TurnState::Player),
+                (update_turn_counter, apply_period)
+                    .chain()
+                    .in_set(TurnSet::Setup),
+            );
     }
 }
 
@@ -34,4 +73,8 @@ pub fn set_player_turn(mut turn_state: ResMut<NextState<TurnState>>) {
 
 fn update_turn_counter(mut turn: ResMut<Turn>) {
     **turn += 1;
+}
+
+fn apply_period(turn: Res<Turn>, mut period_state: ResMut<Period>) {
+    *period_state = Period::from(*turn);
 }
